161fc6ddf7ea33ae5be32eda8662afddc5c6c0ae
Marzio Bordin <marzio@nethesis.it>
2025-10-20 13:31:54 +0200

    Allineamento: aggiornati script Telegram e rcheck_cockpit_sessions.sh

--- rcheck_cockpit_sessions.sh
+++ rcheck_cockpit_sessions.sh
@@ -2,7 +2,7 @@
 # Launcher base per eseguire uno script remoto dal repo GitHub
 # Inserisci l'URL dello script qui sotto
 
-SCRIPT_URL="https://github.com/Coverup20/checkmk-tools/blob/main/script-check-ns7/nopolling/check_cockpit_sessions.sh"
+SCRIPT_URL="https://raw.githubusercontent.com/Coverup20/checkmk-tools/main/script-check-ns7/nopolling/check_cockpit_sessions.sh"
 
 # Puoi passare parametri aggiuntivi allo script remoto
 # Uso: bash rcheck_cockpit_sessions.sh [parametri]
