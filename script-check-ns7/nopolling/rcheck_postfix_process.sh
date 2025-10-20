d732d4f966e4233cedc65d61e42ccccb916b9ab2
Marzio Bordin <marzio@nethesis.it>
2025-10-20 17:21:27 +0200

    Aggiunta launcher r* per tutti gli script e servizi, fix CRLF/LF

---
+++ script-check-ns7/nopolling/rcheck_postfix_process.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Launcher base per eseguire uno script remoto dal repo GitHub
+# Inserisci l'URL dello script qui sotto
+
+SCRIPT_URL="https://raw.githubusercontent.com/Coverup20/checkmk-tools/main/script-check-ns7/nopolling/check_postfix_process.sh"
+
+# Puoi passare parametri aggiuntivi allo script remoto
+# Uso: bash rcheck_postfix_process.sh [parametri]
+
+bash <(curl -fsSL "$SCRIPT_URL") "$@"
---
+++ script-check-ns7/nopolling/rcheck_postfix_status.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Launcher base per eseguire uno script remoto dal repo GitHub
+# Inserisci l'URL dello script qui sotto
+
+SCRIPT_URL="https://raw.githubusercontent.com/Coverup20/checkmk-tools/main/script-check-ns7/nopolling/check_postfix_status.sh"
+
+# Puoi passare parametri aggiuntivi allo script remoto
+# Uso: bash rcheck_postfix_status.sh [parametri]
+
+bash <(curl -fsSL "$SCRIPT_URL") "$@"
